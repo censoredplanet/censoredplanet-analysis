36065a50afc64b7326260588b363dcfcd04ef6db
Sarah Laplante <laplante@google.com>
2020-07-27 11:02:25 -0400

    add date filtering for filenames

--- pipeline/flatten_json.py
+++ pipeline/flatten_json.py
@@ -5,11 +5,15 @@ from __future__ import absolute_import
 import argparse
 import json
 import logging
+import datetime
+import re
 from pprint import pprint
 
 import apache_beam as beam
 from apache_beam.io import ReadFromText
+from apache_beam.io import WriteToText
 from apache_beam.io.gcp.internal.clients import bigquery
+from apache_beam.io.gcp.gcsfilesystem import GCSFileSystem
 from apache_beam.options.pipeline_options import PipelineOptions
 from apache_beam.options.pipeline_options import SetupOptions
 
@@ -58,8 +62,62 @@ def get_bigquery_schema():
   return table_schema
 
 
+def read_scan_text(p, gcs, scan_type, start_date=None, end_date=None):
+  """Read in the given json files for a date segment.
+
+  Args:
+    p: beam pipeline object
+    gcs: GCSFileSystem object
+    scan_type: one of "echo", "discard", "http", "https"
+    start_date: date object, only files at or after this date will be read
+    end_date: date object, only files before or after this date will be read
+
+  Returns:
+    A PCollection<Pair<filename, line>>
+    of all the lines in the files keyed by filename.
+
+    (currently this is stil just a PCollection of lines)
+  """
+  #gcs_reader = GCSFileReader(gcs)
+
+  filename_regex = 'gs://firehook-scans/' + scan_type + '/**/results.json'
+  filenames = (
+      p
+      | 'enumerate files' >> beam.Create(
+          [m.metadata_list for m in gcs.match([filename_regex])])
+      | 'metadata_list to filepath' >> beam.FlatMap(
+          lambda metadata_list: [metadata.path for metadata in metadata_list]))
+
+  filtered_names = (
+      filenames
+      | 'filter file dates' >> beam.Filter(between_dates, start_date, end_date))
+
+  lines = (
+      filtered_names
+      | 'read lines' >> beam.FlatMap(lambda filename: gcs.open(filename)))
+
+  return lines
+
+
+def between_dates(filename, start_date, end_date):
+  """Return true if a filename is between (or matches either of) two dates.
+
+  Args:
+    filename: string of the format
+    "gs://firehook-scans/http/CP_Quack-http-2020-05-11-01-02-08/results.json"
+    start_date: date object
+    end_date: date object
+
+  Returns:
+    boolean
+  """
+  date = datetime.date.fromisoformat(
+      re.findall('\d\d\d\d-\d\d-\d\d', filename)[0])
+  return start_date <= date <= end_date
+
+
 def flatten_measurement(line):
-  """Flattens a measurement string into several roundtrip rows.
+  """Flatten a measurement string into several roundtrip rows.
 
   Args:
     line: a json string describing a censored planet measurement. example
@@ -80,7 +138,7 @@ def flatten_measurement(line):
   try:
     scan = json.loads(line)
   except json.decoder.JSONDecodeError as e:
-    logging.error('%s', e)
+    logging.warn('JSONDecodeError: %s', e)
     return rows
 
   for result in scan['Results']:
@@ -103,45 +161,39 @@ def flatten_measurement(line):
 
 
 def run(argv=None, save_main_session=True):
-  parser = argparse.ArgumentParser()
-  parser.add_argument(
-      '--input',
-      dest='input',
-      default='gs://firehook-scans/echo/**/results.json',
-      #default='gs://firehook-scans/echo/CP_Quack-echo-2018-07-27-15-20-11/results.json',
-      #default='gs://firehook-dataflow-test/results-short*.json',
-      help='Input file to process.')
-  parser.add_argument(
-      '--output',
-      dest='output',
-      default='firehook-censoredplanet:echo_results.scan',
-      help='Output file to write results to.')
-  known_args, pipeline_args = parser.parse_known_args(argv)
-  pipeline_args.extend([
+  pipeline_args = [
       # DataflowRunner or DirectRunner
       '--runner=DataflowRunner',
       '--project=firehook-censoredplanet',
       '--region=us-east1',
       '--staging_location=gs://firehook-dataflow-test/staging',
       '--temp_location=gs://firehook-dataflow-test/temp',
-      '--job_name=flatten-json-job',
-  ])
-
+      '--job_name=flatten-json-https-job',
+  ]
   pipeline_options = PipelineOptions(pipeline_args)
   pipeline_options.view_as(SetupOptions).save_main_session = save_main_session
-  with beam.Pipeline(options=pipeline_options) as p:
+  gcs = GCSFileSystem(PipelineOptions(pipeline_args))
 
-    lines = p | ReadFromText(known_args.input)
+  with beam.Pipeline(options=pipeline_options) as p:
+    scan_type = 'http'
+
+    start_date = datetime.date.fromisoformat('2020-05-07')
+    end_date = datetime.date.fromisoformat('2020-05-11')
+
+    lines = read_scan_text(
+        p, gcs, scan_type, start_date=start_date, end_date=end_date)
 
     rows = (lines | 'flatten json' >> (beam.FlatMap(flatten_measurement)))
 
+    bigquery_table = 'firehook-censoredplanet:' + scan_type + '_results.scan'
+
     rows | 'Write' >> beam.io.WriteToBigQuery(
-        known_args.output,
+        bigquery_table,
         schema=get_bigquery_schema(),
         create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
         # WRITE_TRUNCATE is slow when testing.
-        # write_disposition=beam.io.BigQueryDisposition.WRITE_TRUNCATE
         write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND)
+    #write_disposition=beam.io.BigQueryDisposition.WRITE_TRUNCATE)
 
 
 if __name__ == '__main__':
