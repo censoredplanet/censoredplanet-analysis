aaa8e1680ba8de7784ea9a33b338a76778f4936d
Sarah Laplante <laplante@google.com>
2020-08-07 12:59:26 -0400

    change bulk download script to write to cloud

--- caida_routeviews/bulk_download.py
+++ caida_routeviews/bulk_download.py
@@ -1,40 +1,38 @@
 # to use this script run
 # python3 caida_routeviews/bulk_download.py
-# then run
-# gunzip data/caida/routeviews/*.pfx2as.gz
-# sed -i "s/\t/\//" data/caida/routeviews/*.pfx2as
 
 import subprocess
+import pandas as pd
+from datetime import datetime
 
-# create an all_dates.csv file with all the dates you want to download data for
-with open("./all_dates.csv") as dates:
-  for date in dates:
-    year, month, day = date.strip().split("-")
-    print(day, month, year)
 
+def run():
+  # 2018-07-27 is the earliest scan file date
+  datelist = pd.date_range(start="2018-07-27", end=datetime.today().isoformat())
+
+  for date in datelist:
+    year, month, day = date.isoformat()[:10].split("-")
+    print(year, month, day)
     path = "http://data.caida.org/datasets/routing/routeviews-prefix2as/" + year + "/" + month + "/"
     # possible times are 0000 to 2200 in intervals of 200
-    for time in [
-        "0000", #removing times already looked for
-        "0200",
-        "0400",
-        "0600",
-        "0800",
-        "1000",
-        "1200",
-        "1400",
-        "1600",
-        "1800",
-        "2000",
-        "2200"
-    ]:
+    times = [
+        "0000", "0200", "0400", "0600", "0800", "1000", "1200", "1400", "1600",
+        "1800", "2000", "2200"
+    ]
+    for time in times:
       try:
         filename = "routeviews-rv2-" + year + month + day + "-" + time + ".pfx2as.gz"
-        filename_no_time = "routeviews-rv2-" + year + month + day + ".pfx2as.gz"
 
         print(path + filename)
 
         subprocess.run(["wget", path + filename, "-P", "/tmp/"])
-        subprocess.run(["mv", "/tmp/" + filename, "data/caida/routeviews/" + filename_no_time])
+        subprocess.run([
+            "gsutil", "cp", "/tmp/" + filename,
+            "gs://censoredplanet_geolocation/caida/routeviews/" + filename
+        ])
       except:
         pass
+
+
+if __name__ == "__main__":
+  run()
