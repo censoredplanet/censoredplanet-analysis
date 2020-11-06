67d4d8a37aeae0834ac61246d0d17e0444a13f8c
Sarah Laplante <laplante@google.com>
2020-11-06 18:55:59 -0500

    add an example script for downloading from bq to csv

---
+++ docs/examples/download_to_csv.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Use this script to download data into a csv file for further manual processing
+#
+# This script requires installing the gcloud command line tool for bq.
+# https://cloud.google.com/sdk/gcloud
+
+GLOBIGNORE="*"
+
+# Example query
+# Downloads all HTTP scans from Sudan between 2018-12-01 and 2019-01-30
+query="#standardSQL
+       SELECT *
+       FROM \`firehook-censoredplanet.merged_results.cleaned_error_scans\`
+       WHERE country = 'SD' AND
+             source = 'HTTP' AND
+             '2018-12-01' < date AND date < '2019-01-30'
+       "
+
+bq --apilog=/tmp/query.log --format=csv query "${query}" > sudan_http_data.csv
