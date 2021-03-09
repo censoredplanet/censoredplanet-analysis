f2839b23c820438d7709e596c065f86daf95a8c7
Sarah Laplante <laplante@google.com>
2021-03-09 11:32:47 -0500

    update error classification and documentation

--- table/queries/merged_reduced_scans.sql
+++ table/queries/merged_reduced_scans.sql
@@ -27,64 +27,60 @@ CREATE TEMP FUNCTION CleanError(error STRING) AS (
 # Source is one of "ECHO", "Discard, "HTTP", "HTTPS"
 #
 # Output is a string of the format "step/error_enum"
-#
-# Where step is one of (in order)
-# "setup", "dial", "tls", "write", "read", "http", "content", "final" or "unknown", 
-#
-# and error_enum is an NEL network error type string
-# https://www.w3.org/TR/network-error-logging/#predefined-network-error-types
-# or one of "invalid", "unknown", "tcp.no_route"
-# or a string like "*_mismatch" indicating a successful connection but unexpected content.
+# Documentation of this enum is at
+# https://github.com/Jigsaw-Code/censoredplanet-analysis/blob/master/docs/tables.md#error-classification
 CREATE TEMP FUNCTION ClassifyError(error STRING, source STRING) AS (
   CASE
     # Success
     WHEN (error is NULL OR error = "") then "final/success"
 
     # System failures
-    WHEN ENDS_WITH(error, "address already in use") THEN "setup/invalid"
-    WHEN ENDS_WITH(error, "protocol error") THEN "setup/invalid"
-    WHEN ENDS_WITH(error, "protocol not available") THEN "setup/invalid" # ipv4 vs 6 error
-    WHEN ENDS_WITH(error, "too many open files") THEN "setup/invalid"
+    WHEN ENDS_WITH(error, "address already in use") THEN "setup/system"
+    WHEN ENDS_WITH(error, "protocol error") THEN "setup/system"
+    WHEN ENDS_WITH(error, "protocol not available") THEN "setup/system" # ipv4 vs 6 error
+    WHEN ENDS_WITH(error, "too many open files") THEN "setup/system"
 
     # Dial failures
-    WHEN ENDS_WITH(error, "network is unreachable") THEN "dial/tcp.address_unreachable"
-    WHEN ENDS_WITH(error, "no route to host") THEN "dial/tcp.no_route"
+    WHEN ENDS_WITH(error, "network is unreachable") THEN "dial/ip.network_unreachable"
+    WHEN ENDS_WITH(error, "no route to host") THEN "dial/ip.host_no_route"
     WHEN ENDS_WITH(error, "connection refused") THEN "dial/tcp.refused"
-    WHEN ENDS_WITH(error, "context deadline exceeded") THEN "dial/tcp.timed_out"
-    WHEN ENDS_WITH(error, "connect: connection timed out") THEN "dial/tcp.timed_out"
+    WHEN ENDS_WITH(error, "context deadline exceeded") THEN "dial/timeout"
+    WHEN ENDS_WITH(error, "connect: connection timed out") THEN "dial/timeout"
     WHEN STARTS_WITH(error, "connection reset by peer") THEN "dial/tcp.reset" #no read: or write: prefix in error
     WHEN ENDS_WITH(error, "connect: connection reset by peer") THEN "dial/tcp.reset"
     WHEN ENDS_WITH(error, "getsockopt: connection reset by peer") THEN "dial/tcp.reset"
 
     # TLS failures
-    WHEN REGEXP_CONTAINS(error, "tls:") THEN "tls/tls.protocol.error"
+    WHEN REGEXP_CONTAINS(error, "tls:") THEN "tls/tls.failed"
     WHEN REGEXP_CONTAINS(error, "remote error:") THEN "tls/tls.failed"
     WHEN REGEXP_CONTAINS(error, "local error:") THEN "tls/tls.failed"
     WHEN ENDS_WITH(error, "readLoopPeekFailLocked: <nil>") THEN "tls/tls.failed"
-    WHEN ENDS_WITH(error, "missing ServerKeyExchange message") THEN "tls/tls.protocol.error"
-    WHEN ENDS_WITH(error, "no mutual cipher suite") THEN "tls/tls.protocol.error"
+    WHEN ENDS_WITH(error, "missing ServerKeyExchange message") THEN "tls/tls.failed"
+    WHEN ENDS_WITH(error, "no mutual cipher suite") THEN "tls/tls.failed"
 
     # Write failures
     WHEN ENDS_WITH(error, "write: connection reset by peer") THEN "write/tcp.reset"
-    WHEN ENDS_WITH(error, "write: broken pipe") THEN "write/invalid"
+    WHEN ENDS_WITH(error, "write: broken pipe") THEN "write/system"
 
     # Read failures
-    WHEN REGEXP_CONTAINS(error, "request canceled") THEN "read/tcp.timed_out"
-    WHEN ENDS_WITH(error, "i/o timeout") THEN "read/tcp.timed_out"
-    WHEN ENDS_WITH(error, "shutdown: transport endpoint is not connected") THEN "read/invalid"
+    WHEN REGEXP_CONTAINS(error, "request canceled") THEN "read/timeout"
+    WHEN ENDS_WITH(error, "i/o timeout") THEN "read/timeout"
+    WHEN ENDS_WITH(error, "shutdown: transport endpoint is not connected") THEN "read/system"
     # TODO: for HTTPS this error could potentially also be SNI blocking in the tls stage
     # find a way to diffentiate this case.
     WHEN ENDS_WITH(error, "read: connection reset by peer") THEN "read/tcp.reset"
-    WHEN (source != "ECHO" AND REGEXP_CONTAINS(error, "EOF")) THEN "read/tcp.closed"
-    WHEN ENDS_WITH(error, "http: server closed idle connection") THEN "read/tcp.closed"
+    
 
     # HTTP content verification failures
-    WHEN ENDS_WITH(error, "trailer header without chunked transfer encoding") THEN "http/http.response.invalid"
-    WHEN ENDS_WITH(error, "response missing Location header") THEN "http/http.response.invalid"
-    WHEN REGEXP_CONTAINS(error, "bad Content-Length") THEN "http/http.response.invalid"
-    WHEN REGEXP_CONTAINS(error, "failed to parse Location header") THEN "http/http.response.invalid"
-    WHEN REGEXP_CONTAINS(error, "malformed HTTP") THEN "http/http.response.invalid"
-    WHEN REGEXP_CONTAINS(error, "malformed MIME") THEN "http/http.response.invalid"
+    WHEN (source != "ECHO" AND REGEXP_CONTAINS(error, "unexpected EOF")) THEN "read/http.truncated_response"
+    WHEN (source != "ECHO" AND REGEXP_CONTAINS(error, "EOF")) THEN "read/http.empty"
+    WHEN ENDS_WITH(error, "http: server closed idle connection") THEN "read/http.truncated_response"
+    WHEN ENDS_WITH(error, "trailer header without chunked transfer encoding") THEN "http/http.invalid"
+    WHEN ENDS_WITH(error, "response missing Location header") THEN "http/http.invalid"
+    WHEN REGEXP_CONTAINS(error, "bad Content-Length") THEN "http/http.invalid"
+    WHEN REGEXP_CONTAINS(error, "failed to parse Location header") THEN "http/http.invalid"
+    WHEN REGEXP_CONTAINS(error, "malformed HTTP") THEN "http/http.invalid"
+    WHEN REGEXP_CONTAINS(error, "malformed MIME") THEN "http/http.invalid"
 
     # Content verification failures
     WHEN (source = "ECHO" AND ENDS_WITH(error, "EOF")) THEN "content/response_mismatch" # Echo
@@ -92,8 +88,8 @@ CREATE TEMP FUNCTION ClassifyError(error STRING, source STRING) AS (
     WHEN (error = "Received response") THEN "content/response_mismatch" # Discard
     WHEN (error = "Incorrect web response: status lines don't match") THEN "content/status_mismatch" # HTTP/S
     WHEN (error = "Incorrect web response: bodies don't match") THEN "content/body_mismatch" # HTTP/S
-    WHEN (error = "Incorrect web response: certificates don't match") THEN "content/certificate_mismatch" # HTTPS
-    WHEN (error = "Incorrect web response: cipher suites don't match") THEN "content/cipher_suite_mismatch" # HTTPS
+    WHEN (error = "Incorrect web response: certificates don't match") THEN "content/tls_mismatch" # HTTPS
+    WHEN (error = "Incorrect web response: cipher suites don't match") THEN "content/tls_mismatch" # HTTPS
     WHEN (error = "Incorrect web response: TLS versions don't match") THEN "content/tls_mismatch" # HTTPS
 
     # Unknown errors
