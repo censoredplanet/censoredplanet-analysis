e6b8cb853984bf942e5c22751b5c9d8bdf32bffd
Sarah Laplante <sar.laplante@gmail.com>
2022-02-01 18:40:20 +0000

    set controls_failed to False in satv1 data

--- pipeline/metadata/flatten_satellite.py
+++ pipeline/metadata/flatten_satellite.py
@@ -280,6 +280,7 @@ class SatelliteFlattener():
     row = {
         'domain': scan['query'],
         'is_control': False,  # v1 doesn't have domain controls
+        'controls_failed': False,  # v1 doesn't have domain controls
         'category': self.category_matcher.get_category(scan['query'], False),
         'ip': scan.get('resolver', scan.get('ip')),
         'is_control_ip': filename == SATELLITE_ANSWERS_CONTROL_FILE,
