7a9e807d56e77320ed894cda25f84148c1fd4814
avirkud <avirkud@umich.edu>
2022-04-08 19:53:10 -0400

    export with WriteToFiles, gzip not working :(

--- pipeline/beam_tables.py
+++ pipeline/beam_tables.py
@@ -19,12 +19,16 @@ import datetime
 import logging
 import pathlib
 import re
+import json
 from typing import Optional, Tuple, Dict, List, Any, Iterator, Iterable
 
 import apache_beam as beam
 from apache_beam.io.gcp.gcsfilesystem import GCSFileSystem
+from apache_beam.io.filesystem import CompressionTypes
+from apache_beam.io.fileio import WriteToFiles
 from apache_beam.options.pipeline_options import PipelineOptions
 from apache_beam.options.pipeline_options import SetupOptions
+from apache_beam.transforms.window import GlobalWindow
 from google.cloud import bigquery as cloud_bigquery  # type: ignore
 
 from pipeline.metadata.beam_metadata import DateIpKey, IP_METADATA_PCOLLECTION_NAME, ROWS_PCOLLECION_NAME, make_date_ip_key, merge_metadata_with_rows
@@ -423,6 +427,91 @@ class ScanDataBeamPipelineRunner():
         write_disposition=write_mode,
         additional_bq_parameters=_get_partition_params()))
 
+  def _write_to_gcs(self, scan_type: str,
+                         rows: beam.pvalue.PCollection[BigqueryRow]) -> None:
+    """Write out rows to hive-partitioned GCS folder.
+
+    Args:
+      scan_type: one of 'echo', 'discard', 'http', 'https',
+        or 'satellite'
+      rows: PCollection[BigqueryRow] of data to write.
+
+    Raises:
+      Exception: if any arguments are invalid.
+    """
+    def get_destination(record: str):
+      record_dict = json.loads(record)
+      return f'scan_type={scan_type}/date={record_dict["date"]}/country={record_dict["country"]}/results.json.gz'
+    
+    # Modified this function to get rid of the default shard naming.
+    # TODO: clean up
+    def custom_file_naming(suffix=None):
+      def _inner(window, pane, shard_index, total_shards, compression, destination):
+        prefix = str(destination)
+        DEFAULT = '{prefix}-{start}-{end}-{pane}-{shard:05d}-of-{total_shards:05d}{suffix}{compression}'
+        kwargs = {
+            'prefix': prefix,
+            'start': '',
+            'end': '',
+            'pane': '',
+            'shard': 0,
+            'total_shards': 0,
+            'suffix': '',
+            'compression': ''
+        }
+        if total_shards is not None and shard_index is not None:
+          kwargs['shard'] = int(shard_index)
+          kwargs['total_shards'] = int(total_shards)
+
+        if window != GlobalWindow():
+          kwargs['start'] = window.start.to_utc_datetime().isoformat()
+          kwargs['end'] = window.end.to_utc_datetime().isoformat()
+        
+        if suffix:
+          kwargs['suffix'] = suffix
+
+        if compression:
+          kwargs['compression'] = '.%s' % compression
+
+        logging.info(kwargs)
+        # Remove separators for unused template parts.
+        format = DEFAULT
+        if shard_index is None or kwargs['total_shards'] <= 1:
+          format = format.replace('-{shard:05d}', '')
+        if total_shards is None or kwargs['total_shards'] <= 1:
+          format = format.replace('-of-{total_shards:05d}', '')
+        for name, value in kwargs.items():
+          if value in (None, ''):
+            format = format.replace('-{%s}' % name, '')
+
+        return format.format(**kwargs)
+
+      return _inner
+    # Pcollection[Dict[str, Any]]
+    dict_rows = (
+        rows | f'dataclass to dicts {scan_type}' >> beam.Map(
+            schema.flatten_for_bigquery).with_output_types(Dict[str, Any]))
+
+    # PCollection[str]
+    json_rows = (
+        dict_rows | 'dicts to json' >> beam.Map(json.dumps).with_output_types(str)
+    )
+
+    path = 'gs://firehook-test/test/'
+    sink = beam.io.textio._TextSink(path,
+            file_name_suffix='.json.gz',
+            compression_type=CompressionTypes.GZIP,
+            num_shards=1)
+    sink.mime_type = 'application/json'
+
+    (json_rows | 'Write to GCS files' >> WriteToFiles(
+          path=path,
+          destination=lambda record: get_destination(record),
+          sink=sink,
+          shards=1,
+          file_naming=custom_file_naming()
+    ))
+
   def _get_pipeline_options(self, scan_type: str,
                             job_name: str) -> PipelineOptions:
     """Sets up pipeline options for a beam pipeline.
@@ -453,7 +542,8 @@ class ScanDataBeamPipelineRunner():
   def run_beam_pipeline(self, scan_type: str, incremental_load: bool,
                         job_name: str, table_name: str,
                         start_date: Optional[datetime.date],
-                        end_date: Optional[datetime.date]) -> None:
+                        end_date: Optional[datetime.date],
+                        export_gcs: bool) -> None:
     """Run a single apache beam pipeline to load json data into bigquery.
 
     Args:
@@ -466,6 +556,8 @@ class ScanDataBeamPipelineRunner():
         Mostly only used during development.
       end_date: date object, only files at or before this date will be read.
         Mostly only used during development.
+      export_gcs: boolean. If true, write to Google Cloud Storage, if false
+        write to BigQuery.
 
     Raises:
       Exception: if any arguments are invalid or the pipeline fails.
@@ -499,5 +591,8 @@ class ScanDataBeamPipelineRunner():
 
       _raise_error_if_collection_empty(rows_with_metadata)
 
-      self._write_to_bigquery(scan_type, rows_with_metadata, table_name,
-                              incremental_load)
+      if export_gcs:
+        self._write_to_gcs(scan_type, rows_with_metadata)
+      else:
+        self._write_to_bigquery(scan_type, rows_with_metadata, table_name,
+                                incremental_load)
--- pipeline/run_beam_tables.py
+++ pipeline/run_beam_tables.py
@@ -31,7 +31,8 @@ def run_parallel_pipelines(runner: beam_tables.ScanDataBeamPipelineRunner,
                            dataset: str, scan_types: List[str],
                            incremental_load: bool,
                            start_date: Optional[datetime.date],
-                           end_date: Optional[datetime.date]) -> bool:
+                           end_date: Optional[datetime.date],
+                           export_gcs: bool) -> bool:
   """Runs beam pipelines for different scan types in parallel.
 
   Args:
@@ -44,6 +45,8 @@ def run_parallel_pipelines(runner: beam_tables.ScanDataBeamPipelineRunner,
       Mostly only used during development.
     end_date: date object, only files at or before this date will be read.
       Mostly only used during development.
+    export_gcs: boolean. If true, write to Google Cloud Storage, if false
+      write to BigQuery.
 
   Returns:
     True on success
@@ -57,10 +60,12 @@ def run_parallel_pipelines(runner: beam_tables.ScanDataBeamPipelineRunner,
       table_name = beam_tables.get_table_name(dataset, scan_type,
                                               beam_tables.BASE_TABLE_NAME)
       job_name = beam_tables.get_job_name(table_name, incremental_load)
+      if export_gcs:
+        job_name += '_gcs'
 
       future = pool.submit(runner.run_beam_pipeline, scan_type,
                            incremental_load, job_name, table_name, start_date,
-                           end_date)
+                           end_date, export_gcs)
       futures.append(future)
 
     finished, pending = concurrent.futures.wait(
@@ -113,11 +118,13 @@ def main(parsed_args: argparse.Namespace) -> None:
   if parsed_args.env == 'user':
     run_parallel_pipelines(firehook_runner, parsed_args.user_dataset,
                            selected_scan_types, incremental,
-                           parsed_args.start_date, parsed_args.end_date)
+                           parsed_args.start_date, parsed_args.end_date,
+                           parsed_args.export_gcs)
   elif parsed_args.env == 'prod':
     run_parallel_pipelines(firehook_runner, beam_tables.PROD_DATASET_NAME,
                            selected_scan_types, incremental,
-                           parsed_args.start_date, parsed_args.end_date)
+                           parsed_args.start_date, parsed_args.end_date,
+                           parsed_args.export_gcs)
 
 
 def parse_args() -> argparse.Namespace:
@@ -158,6 +165,11 @@ def parse_args() -> argparse.Namespace:
       default=None,
       metavar='yyyy-mm-dd',
       help='Last date to read data from')
+  parser.add_argument(
+      '--export_gcs',
+      action='store_true',
+      default=False,
+      help='Export to Google Cloud Storage instead of BigQuery')
   parsed_args = parser.parse_args()
 
   if (parsed_args.env == 'user' and parsed_args.user_dataset is None):
