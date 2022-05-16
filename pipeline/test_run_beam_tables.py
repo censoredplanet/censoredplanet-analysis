9d0d8f852ba636d7cd719603397f697d04a2f354
Sarah Laplante <sar.laplante@gmail.com>
2022-05-16 08:51:35 +0000

    turn satellite pipeline on in production

--- pipeline/test_run_beam_tables.py
+++ pipeline/test_run_beam_tables.py
@@ -81,10 +81,12 @@ class RunBeamTablesTest(unittest.TestCase):
                    None, None)
       call4 = call('https', True, 'append-base-https-scan', 'base.https_scan',
                    None, None)
+      call5 = call('satellite', True, 'append-base-satellite-scan',
+                   'base.satellite_scan', None, None)
       mock_runner.run_beam_pipeline.assert_has_calls(
-          [call1, call2, call3, call4], any_order=True)
+          [call1, call2, call3, call4, call5], any_order=True)
       # No extra calls
-      self.assertEqual(4, mock_runner.run_beam_pipeline.call_count)
+      self.assertEqual(5, mock_runner.run_beam_pipeline.call_count)
 
   def test_main_user_dates(self) -> None:
     """Test arg parsing for a user pipeline with dates."""
