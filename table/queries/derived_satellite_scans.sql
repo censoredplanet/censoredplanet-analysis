c362dcdb7296ba2fdfa661102f0ced1d92f8dd5c
Sarah Laplante <sar.laplante@gmail.com>
2022-12-09 20:30:06 +0000

    convert all satellite outcomes to ! except timeout/not_validated/servfail

--- table/queries/derived_satellite_scans.sql
+++ table/queries/derived_satellite_scans.sql
@@ -1,30 +1,30 @@
 # https://www.iana.org/assignments/dns-parameters/dns-parameters.xhtml#dns-parameters-6
 CREATE TEMP FUNCTION ClassifySatelliteRCode(rcode INTEGER) AS (
   CASE
-    WHEN rcode = 1 THEN "❓dns/rcode:FormErr"
+    WHEN rcode = 1 THEN "❗️dns/rcode:FormErr"
     WHEN rcode = 2 THEN "❓dns/rcode:ServFail"
     WHEN rcode = 3 THEN "❗️dns/rcode:NXDomain"
-    WHEN rcode = 4 THEN "❓dns/rcode:NotImp"
+    WHEN rcode = 4 THEN "❗️dns/rcode:NotImp"
     WHEN rcode = 5 THEN "❗️dns/rcode:Refused"
-    WHEN rcode = 6 THEN "❓dns/rcode:YXDomain"
-    WHEN rcode = 7 THEN "❓dns/rcode:YXRRSet"
-    WHEN rcode = 8 THEN "❓dns/rcode:NXRRSet"
-    WHEN rcode = 9 THEN "❓dns/rcode:NotAuth"
-    WHEN rcode = 10 THEN "❓dns/rcode:NotZone"
-    WHEN rcode = 11 THEN "❓dns/rcode:DSOTYPENI"
-    WHEN rcode = 12 THEN "❓dns/rcode:Unassigned"
-    WHEN rcode = 13 THEN "❓dns/rcode:Unassigned"
-    WHEN rcode = 14 THEN "❓dns/rcode:Unassigned"
-    WHEN rcode = 15 THEN "❓dns/rcode:Unassigned"
-    WHEN rcode = 16 THEN "❓dns/rcode:BadVers"
-    WHEN rcode = 17 THEN "❓dns/rcode:BadSig"
-    WHEN rcode = 18 THEN "❓dns/rcode:BadKey"
-    WHEN rcode = 19 THEN "❓dns/rcode:BadTime"
-    WHEN rcode = 20 THEN "❓dns/rcode:BadMode"
-    WHEN rcode = 21 THEN "❓dns/rcode:BadAlg"
-    WHEN rcode = 22 THEN "❓dns/rcode:BadTrunc"
-    WHEN rcode = 23 THEN "❓dns/rcode:BadCookie"
-    ELSE CONCAT("❓dns/unknown_rcode:", rcode)
+    WHEN rcode = 6 THEN "❗️dns/rcode:YXDomain"
+    WHEN rcode = 7 THEN "❗️dns/rcode:YXRRSet"
+    WHEN rcode = 8 THEN "❗️dns/rcode:NXRRSet"
+    WHEN rcode = 9 THEN "❗️dns/rcode:NotAuth"
+    WHEN rcode = 10 THEN "❗️dns/rcode:NotZone"
+    WHEN rcode = 11 THEN "❗️dns/rcode:DSOTYPENI"
+    WHEN rcode = 12 THEN "❗️dns/rcode:Unassigned"
+    WHEN rcode = 13 THEN "❗️dns/rcode:Unassigned"
+    WHEN rcode = 14 THEN "❗️dns/rcode:Unassigned"
+    WHEN rcode = 15 THEN "❗️dns/rcode:Unassigned"
+    WHEN rcode = 16 THEN "❗️dns/rcode:BadVers"
+    WHEN rcode = 17 THEN "❗️dns/rcode:BadSig"
+    WHEN rcode = 18 THEN "❗️dns/rcode:BadKey"
+    WHEN rcode = 19 THEN "❗️dns/rcode:BadTime"
+    WHEN rcode = 20 THEN "❗️dns/rcode:BadMode"
+    WHEN rcode = 21 THEN "❗️dns/rcode:BadAlg"
+    WHEN rcode = 22 THEN "❗️dns/rcode:BadTrunc"
+    WHEN rcode = 23 THEN "❗️dns/rcode:BadCookie"
+    ELSE CONCAT("❗️dns/unknown_rcode:", rcode)
   END
 );
 
@@ -32,21 +32,21 @@ CREATE TEMP FUNCTION ClassifySatelliteError(error STRING) AS (
   CASE
     # Satellite v1
     WHEN REGEXP_CONTAINS(error, '"Err": {}') THEN "❓read/udp.timeout"
-    WHEN REGEXP_CONTAINS(error, '"Err": 90') THEN "❓read/dns.msgsize"
-    WHEN REGEXP_CONTAINS(error, '"Err": 111') THEN "❓read/udp.refused"
-    WHEN REGEXP_CONTAINS(error, '"Err": 113') THEN "❓read/ip.host_no_route"
+    WHEN REGEXP_CONTAINS(error, '"Err": 90') THEN "❗️read/dns.msgsize"
+    WHEN REGEXP_CONTAINS(error, '"Err": 111') THEN "❗️read/udp.refused"
+    WHEN REGEXP_CONTAINS(error, '"Err": 113') THEN "❗️read/ip.host_no_route"
     WHEN REGEXP_CONTAINS(error, '"Err": 24') THEN "❔setup/system_failure" # Too many open files
-    WHEN error = "{}" THEN "❓dns/unknown" # TODO figure out origin
-    WHEN error = "no_answer" THEN "❓dns/answer:no_answer"
+    WHEN error = "{}" THEN "❗️dns/unknown" # TODO figure out origin
+    WHEN error = "no_answer" THEN "❗️dns/answer:no_answer"
     #Satellite v2
     WHEN ENDS_WITH(error, "i/o timeout") THEN "❓read/udp.timeout"
-    WHEN ENDS_WITH(error, "message too long") THEN "❓read/dns.msgsize"
-    WHEN ENDS_WITH(error, "connection refused") THEN "❓read/udp.refused"
-    WHEN ENDS_WITH(error, "no route to host") THEN "❓read/ip.host_no_route"
-    WHEN ENDS_WITH(error, "short read") THEN "❓read/dns.msgsize"
-    WHEN ENDS_WITH(error, "read: protocol error") THEN "❓read/protocol_error"
+    WHEN ENDS_WITH(error, "message too long") THEN "❗️read/dns.msgsize"
+    WHEN ENDS_WITH(error, "connection refused") THEN "❗️read/udp.refused"
+    WHEN ENDS_WITH(error, "no route to host") THEN "❗️read/ip.host_no_route"
+    WHEN ENDS_WITH(error, "short read") THEN "❗️read/dns.msgsize"
+    WHEN ENDS_WITH(error, "read: protocol error") THEN "❗️read/protocol_error"
     WHEN ENDS_WITH(error, "socket: too many open files") THEN "❔setup/system_failure"
-    ELSE CONCAT("❓dns/unknown_error:", error)
+    ELSE CONCAT("❗️dns/unknown_error:", error)
   END
 );
 
@@ -103,7 +103,7 @@ CREATE TEMP FUNCTION OutcomeString(domain_name STRING,
         # TODO fix -1 rcodes in v1 data in the pipeline
         WHEN rcode = -1 THEN "❓read/udp.timeout"
         WHEN rcode != 0 THEN ClassifySatelliteRCode(rcode)
-        WHEN ARRAY_LENGTH(answers) = 0 THEN "❓answer:no_answer"
+        WHEN ARRAY_LENGTH(answers) = 0 THEN "❗️answer:no_answer"
         ELSE IFNULL(
             (SELECT InvalidIpType(answer.ip) FROM UNNEST(answers) answer LIMIT 1),
             CASE
