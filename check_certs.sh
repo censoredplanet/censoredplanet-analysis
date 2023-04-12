ce95797c69205f196fca564fd193683dfa91b210
Sarah Laplante <sar.laplante@gmail.com>
2023-04-12 15:18:43 +0000

    add outputs

--- check_certs.sh
+++ check_certs.sh
@@ -3,6 +3,6 @@ touch output.txt
 while read p; do
   echo $p
   echo https://$p
-  echo $p >> output.txt
-  curl -m 15 -w "\n%{http_code}\n" https://$p | tail -n 1 >> output.txt
+  echo $p\| >> output.txt
+  curl -m 15 -w "\n%{http_code},%{exitcode},%{errormsg}\n" https://$p | tail -n 1 >> output.txt
 done < all_sat_domains.csv
